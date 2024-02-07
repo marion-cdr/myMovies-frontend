a275ef7e0e432ee270f3a5b3b310324f57c782ad
Marion <marion.1701@hotmail.fr>
2024-02-07 16:03:06 +0100

    first commit

--- components/Home.js
+++ components/Home.js
@@ -44,7 +44,7 @@ function Home() {
   // ];
 
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://my-movies-backend-zeta.vercel.app/movies')
       .then(res => res.json())
       .then(data => {
         setMoviesData(data.movies);
